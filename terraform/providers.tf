efa4f14a3ad1b77b5d56c5595bbb67ef569751f6
Destiny Malone <malone.destiny34@gmail.com>
2025-06-06 14:55:35 +0700

    Updated aws, random, and terraform versions in providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,15 +2,15 @@ terraform {
     required_providers {
         aws = {
         source  = "hashicorp/aws"
-        version = ">= 5.0, < 6.0"
+        version = ">= 5.52.0"
         }
         random = {
         source  = "hashicorp/random"
-        version = "~> 3.0"
+        version = ">= 3.6.2"
         }
     }
     
-    required_version = ">= 1.4.0"
+    required_version = ">= 1.8.4"
 }
 provider "aws" {
     region  = var.aws_region
